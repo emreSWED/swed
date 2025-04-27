f976e7eef9f585ee0c070f879fddff434857c813
emreSWED <emre.oezoener@stud.fra-uas.de>
2025-04-27 14:34:11 +0200

    Update collatz.cpp

--- collatz.cpp
+++ collatz.cpp
@@ -3,18 +3,10 @@
 #include <cstdio>
 int main(){
     for(unsigned long int i = 1; i <= 1000000; i++){
-        printf("current i number = %llu\n", i);
         unsigned long int testing = i;
-        
-
         while(testing != 1){
-        printf("current testing number = %llu\n", testing);
-        if(testing%2 == 0){
-            testing = testing/2;
-        } else {
-            testing = (testing*3) + 1;
-        }
-        }
+        if(testing%2 == 0)    testing =/2; 
+        else                  testing = (testing*3) + 1;
     }
 printf("No counterexample found until n = 1.000.000");
 return 0;
